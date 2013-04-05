03ac97ae23f5fdb73e8ce22c69f8a28ab20190ae
Hendrik <hendrik.cech@gmail.com>
2013-04-05 18:53:01 +0200

    clean up

--- auth.js
+++ auth.js
@@ -2,7 +2,12 @@ var request = require('tent-request')
 
 /* registers app and builds an url where the user can authorize the app */
 exports.generateUrl = function(server, app, callback) {
-	
+	if(!server) throw new Error("argument 'server' required")
+	if(!app) throw new Error("argument 'app' required")
+	if(!(app.name && app.description && app.url && app.redirect_uris && app.scopes)) {
+		throw new Error("missing required app infos (more information: https://tent.io/docs/app-auth#app-json-schema")
+	}
+
 	var content = {
 		'name': app.name,
 		'description': app.description,
@@ -12,42 +17,46 @@ exports.generateUrl = function(server, app, callback) {
 		'scopes': app.scopes
 	}
 
-	request('post', 'https://hendrik.tent.is/tent/apps', null, content, function(err, body) {
-			if(err) return callback(err)
+	request({
+		method: 'post',
+		url: server + '/apps',
+		param: content
+	}, function(err, res, body) {
+		if(err) return callback(err)
+		if(res.statusCode < 200 || res.statusCode >= 300) return callback('bad statusCode: ' + res.statusCode)
 
-			var keys = {
-				'id': body.id, //id of app
-				'mac_key_id': body.mac_key_id, //valid during auth process
-				'mac_key': body.mac_key, //same
-				'mac_algorithm': body.mac_algorithm //sisi
-			}
-			
-			/* builds a url where the user cann authorize the app */
-			var scopes = []
-			for(var key in app.scopes) {
-				scopes.push(key)
-			}
-			var scope = scopes.join(',')
-
-			//create nonce http://www.mediacollege.com/internet/javascript/number/random.html
-			var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz"
-			var state = ''
-			for (var i=0; i<6; i++) {
-				var rnum = Math.floor(Math.random() * chars.length)
-				state += chars.substring(rnum,rnum+1)
-			}
-
-			var url = server + '/oauth/authorize?'
-				+ 'client_id=' + keys.id
-				+ '&scope=' + scope
-				+ '&state=' + state
-				+ '&redirect_uri=' + app.redirect_uris[0]
-				+ '&tent_profile_info_types=' + app.tent_profile_info_types
-				+ '&tent_post_types=' + app.tent_post_types
-
-			callback(null, url, keys, state)
+		var keys = {
+			'id': body.id, //id of app
+			'mac_key_id': body.mac_key_id, //valid during auth process
+			'mac_key': body.mac_key, //same
+			'mac_algorithm': body.mac_algorithm //sisi
 		}
-	)
+		
+		/* builds a url where the user cann authorize the app */
+		var scopes = []
+		for(var key in app.scopes) {
+			scopes.push(key)
+		}
+		var scope = scopes.join(',')
+
+		//create nonce http://www.mediacollege.com/internet/javascript/number/random.html
+		var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz"
+		var state = ''
+		for (var i=0; i<6; i++) {
+			var rnum = Math.floor(Math.random() * chars.length)
+			state += chars.substring(rnum,rnum+1)
+		}
+
+		var url = server + '/oauth/authorize?'
+			+ 'client_id=' + keys.id
+			+ '&scope=' + scope
+			+ '&state=' + state
+			+ '&redirect_uri=' + app.redirect_uris[0]
+			+ '&tent_profile_info_types=' + app.tent_profile_info_types
+			+ '&tent_post_types=' + app.tent_post_types
+
+		callback(null, url, keys, state)
+	})
 }
 
 /* last authentication step; 'trades' received code for a persistive token */
@@ -69,8 +78,14 @@ exports.tradeCode = function(server, keys, code, callback) {
 		macKey: keys.mac_key,
 		macKeyId: keys.mac_key_id
 	}
-	request('post', server + path, keys, {code: code, 'token_type': 'mac'}, function(err, token) {
+	request({
+		method: 'post',
+		url: server + path,
+		auth: keys,
+		param: {code: code, 'token_type': 'mac'}
+	}, function(err, res, token) {
 		if(err) return callback(err)
+		if(res.statusCode < 200 || res.statusCode >= 300) return callback('bad statusCode: ' + res.statusCode)
 		callback(null, token)
 	})
 }
