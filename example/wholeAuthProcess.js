3228f98c340292866bc70b87403a62768f709e70
Hendrik <hendrik.cech@gmail.com>
2013-04-07 23:11:41 +0200

    updated example to use readline instead of cli-prompt

--- example/wholeAuthProcess.js
+++ example/wholeAuthProcess.js
@@ -1,8 +1,14 @@
-//this example also uses the tent-discover and cli-prompt modules!
+// this example also uses the tent-discover module!
+// => npm install tent-discover
 
 var discover = require('tent-discover')
-var auth = require('tent-auth')
-var prompt = require('cli-prompt')
+var auth = require('../auth')
+var readline = require('readline')
+
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
 
 var app = {
 	"name": "FooApp",
@@ -22,9 +28,10 @@ var app = {
 	"tent_post_types": "https://tent.io/types/posts/status/v0.1.0"
 }
 
-var server = ''
+var entity = 'https://hendrik.tent.is' // change this to the desired entity
 
-discover('https://example.tent.is', function(err, profile) {
+var server;
+discover(entity, function(err, profile) {
 	if(err) return console.log(err)
 
 	server = profile['https://tent.io/types/info/core/v0.1.0']['servers'][0]
@@ -36,11 +43,12 @@ function proceed(err, url, appKeys, state) {
 	if(err) return console.log(err)
 
 	console.log('Navigate to:\n' + url)
-	prompt('code:', function(code) {
+	rl.question('code:', function(code) {
 		auth.tradeCode(server, appKeys, code, function(err, token) {
 			if(err) return console.log(err)
 
 			console.log(token)
+			rl.close()
 		})
 	})
 }
